e090a9432ce0330b6eb489c7ee2e2dadeff48438
dxcufgb <sten.alf.tom.pettersson@gmail.com>
2025-12-20 15:48:15 +0100

    [AH] add createFeature script

---
+++ createFeature.js
@@ -0,0 +1,19 @@
+Hooks.once("init", function(){
+	CONFIG.DND5E.featureTypes.transformation = {
+		label: "Transformation Feature",
+		subtypes: {
+			aberrantHorror: "Aberrant Horror",
+			fey: "Fey",
+			fiend: "Fiend",
+			hag: "Hag",
+			lich: "Lich",
+			lycanthrope: "Lycanthrope",
+			ooze: "Ooze",
+			primordial: "Primordial",
+			seraph: "Seraph",
+			shadowsteelGhoul: "Shadowsteel Ghoul",
+			specter: "Specter",
+			vampire: "Vampire"
+		}
+	}
+});
