929383c05065449c8773233ba4a6705990db9f02
dxcufgb <dxcufgb@users.noreply.github.com>
2025-12-21 22:23:25 +0100

    [AH] lots of small fixes

--- unstable-form-roll-table-macros.js
+++ unstable-form-roll-table-macros.js
@@ -1,9 +1,9 @@
 function applyUnstableForm(actor, effectName) {
 	return new Promise((resolve) => {
-		let effectDescription = ''
-		let iconFilePath = ''
-		let effects = []
-		let runEffectsFunction = true
+		let effectDescription = '';
+		let iconFilePath = '';
+		let effects = [];
+		let runEffectsFunction = true;
 		switch (effectName) {
 			case "Aberrant Exhaustion":
 				const currentExhaustion = actor.system.attributes.exhaustion ?? 0;
@@ -11,7 +11,7 @@ function applyUnstableForm(actor, effectName) {
 				actor.update({
 					"system.attributes.exhaustion": newExhaustion
 				});
-				runEffectsFunction = false
+				runEffectsFunction = false;
 				break;
 			case "Aberrant Confusion":
 				effectDescription = "After rolling Initiative, you have the Stunned condition until the end of your first turn";
@@ -65,16 +65,16 @@ function applyUnstableForm(actor, effectName) {
 				break;
 			case "Aberrant Temporary Vitality Boost":
 				const currentTempHp = actor.system.attributes.hp.temp ?? 0;
-				const newTempHp = currentTempHp + (actor.system.scale["aberrant-horror"]["transformation-level"].value * 4)
+				const newTempHp = currentTempHp + (actor.system.scale["aberrant-horror"]["transformation-level"].value * 4);
 				actor.update({
 					"system.attributes.hp.temp": newTempHp
 				});
-				runEffectsFunction = false
+				runEffectsFunction = false;
 				break;
 			case "Aberrant Resilience":
 				effectDescription = "Your body’s systems are enhanced. You have Advantage on Death Saving Throws";
 				iconFilePath = "icons/svg/poison.svg";
-				effects.push(getAdvantageEffectChanges(ATTRIBUTES.DEATH_SAVES, ROLL_TYPE.SAVING_THROW))
+				effects.push(getAdvantageEffectChanges(ATTRIBUTES.DEATH_SAVES, ROLL_TYPE.SAVING_THROW));
 				break;
 			case "Aberrant Overload":
 				effectDescription = "The stress of your Transformation becomes too much. You die. You cannot be restored to life by any spell below level 5";
@@ -95,29 +95,29 @@ function applyUnstableForm(actor, effectName) {
 				runEffectsFunction = false
 				break;
 			case "Aberrant Weakness":
-				effectDescription = "Your form becomes fragile. Your Hit Point Maximum is half your normal maximum"
+				effectDescription = "Your form becomes fragile. Your Hit Point Maximum is half your normal maximum";
 				iconFilePath = "icons/svg/poison.svg";
-				const newMaxHp = (actor.system.attributes.hp.max / 2)
+				const newMaxHp = (actor.system.attributes.hp.max / 2);
 				effects.push(getSystemEffectChange(ATTRIBUTES.HEALT_POINTS_MAX, newMaxHp, CONST.ACTIVE_EFFECT_MODES.OVERRIDE));
 				break;
 			case "Aberrant Weakness":
-				effectDescription = "Your body starts to lose cohesion. You have Disadvantage on all D20 Tests."
+				effectDescription = "Your body starts to lose cohesion. You have Disadvantage on all D20 Tests.";
 				iconFilePath = "icons/svg/poison.svg";
 				Object.values(SKILL).forEach(skill => {
 					effects.push(getSkillDisadvantageEffectChanges(skill));
 				});
 				Object.values(ABILITY).forEach(ability => {
-					effects.push(getAbilityCheckDisadvantageEffectChanges(ability))
-					effects.push(getAbilitySaveDisadvantageEffectChanges(ability))
+					effects.push(getAbilityCheckDisadvantageEffectChanges(ability));
+					effects.push(getAbilitySaveDisadvantageEffectChanges(ability));
 				});
 				Object.values(ATTRIBUTES.ROLLABLE).forEach(attribute => {
-					effects.push(getAttributeCheckDisadvantageEffectChanges(attribute))
-					effects.push(getAttributeSaveDisadvantageEffectChanges(attribute))
-				})
+					effects.push(getAttributeCheckDisadvantageEffectChanges(attribute));
+					effects.push(getAttributeSaveDisadvantageEffectChanges(attribute));
+				});
 				break;
 		}
 		if (runEffectsFunction) {
-			createActiveEffectOnActor(actor, effectName, effectDescription, iconFilePath, effects)
+			createActiveEffectOnActor(actor, effectName, effectDescription, iconFilePath, effects);
 		}
 		resolve(true);
 	});
