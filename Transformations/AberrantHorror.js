a39be4954f9b880ef9637f21af06bddfc1508c97
dxcufgb <dxcufgb@users.noreply.github.com>
2025-12-25 23:36:37 +0100

    [AH] attempt to solve name lookup in removeEldritchLims

--- Transformations/AberrantHorror.js
+++ Transformations/AberrantHorror.js
@@ -194,18 +194,8 @@ export class AberrantHorror extends TransformationModule.TransformationParent.Tr
     async removeEldritchLimbsItems() {
         let itemsToRemove = [];
         for (let index = 1; index <= 2; index++) {
-            const itemId = this.eldritchLimbsItemIds[index];
-            const itemNameToLookFor = (await fromUuid(itemId)).name
-            console.log("item to look for:");
-            console.log(itemNameToLookFor)
-            itemsToRemove.push(
-                this.actor.items.filter(i =>
-                    i.name == itemNameToLookFor
-                )
-            );
+            itemsToRemove.push(this.eldritchLimbsItemIds[index]);
         }
-        console.log("removing items:");
-        console.log(itemsToRemove);
         await this.actor.deleteEmbeddedDocuments(
             "Item",
             itemsToRemove
