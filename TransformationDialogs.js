70093f4edf5c6d6f45976677feb3ab5bcc3ef74c
dxcufgb <dxcufgb@users.noreply.github.com>
2025-12-26 14:29:48 +0100

    [AH] dialogs adjustment

--- TransformationDialogs.js
+++ TransformationDialogs.js
@@ -1,21 +1,21 @@
-export async function getRollDialogConfig(actor, savingThrowIdentifier, title = "Roll") {
-    let config;;
-    if (actor){
-    config = await dnd5e.buildSavingThrowRollConfig(actor, savingThrowIdentifier, {
-        title: title
-    });
+export async function getD20Roll(actor, identifier, title, flavor, mode = TransformationModule.constants.ROLL_MODE.NORMAL) {
+    if (!actor) {
+        ui.notifications.warn("Select a token.");
+        return;
     }
-    return config;
-}
 
-export function getSimpleDialog(title, content) {
-    return new Dialog({
-        title: title,
-        content: `<p>${content}</p>`,
-        buttons: {
-            ok: {
-                label: "Ok",
-            }
-        }
+    const config = await dnd5e.buildSavingThrowRollConfig(actor, identifier, {
+    title: title,
+    flavor: flavor
     });
+
+    if (mode == TransformationModule.constants.ROLL_MODE.ADVANTAGE) {
+        config.advantage = true
+    }
+
+    if (mode == TransformationModule.constants.ROLL_MODE.DISADVANTAGE) {
+        config.disadvantage = true
+    }
+
+    const roll = await dnd5e.rollSavingThrow(config);
 }
