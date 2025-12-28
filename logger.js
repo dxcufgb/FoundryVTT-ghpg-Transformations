812a46d2efc2ebc09cfcc708410f1c1f95b11933
dxcufgb <dxcufgb@users.noreply.github.com>
2025-12-28 23:36:57 +0100

    [AH] added the logger.js file

---
+++ logger.js
@@ -0,0 +1,39 @@
+export class Logger{
+    prefix = "Transformations |";
+    logLevel;
+
+    constructor(logLevel = 1) {
+        this.prefix = this.constructor.prefix;
+        this.logLevel = logLevel;
+    }
+    
+    debug(...args) {
+        if (this.logLevel = 5) {
+            console.debug(prefix, ...args);
+        }
+    }
+    
+    info(...args) {
+        if (this.logLevel = 4) {
+            console.info(prefix, ...args);
+        }
+    }
+    
+    log(...args) {
+        if (this.logLevel = 3) {
+            console.log(prefix, ...args);
+        }
+    }
+    
+    warn(...args) {
+        if (this.logLevel = 2) {
+            console.warn(prefix, ...args);
+        }
+    }
+    
+    error(...args) {
+        if (this.logLevel = 1) {
+            console.error(prefix, ...args);
+        }
+    }
+}
