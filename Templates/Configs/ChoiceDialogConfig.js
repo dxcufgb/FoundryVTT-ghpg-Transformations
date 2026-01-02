8d4d1c129d5f95e3210e852dc282a633b1d1c340
dxcufgb <dxcufgb@users.noreply.github.com>
2026-01-03 00:59:10 +0100

    [AH] attempt to make a choice dialog

--- Templates/Configs/ChoiceDialogConfig.js
+++ Templates/Configs/ChoiceDialogConfig.js
@@ -1,24 +1,21 @@
-export class TransformationConfig extends foundry.applications.api.HandlebarsApplicationMixin(foundry.applications.api.ApplicationV2) {
+export class ChoiceDialogConfig extends foundry.applications.api.HandlebarsApplicationMixin(foundry.applications.api.ApplicationV2) {
     static DEFAULT_OPTIONS = {
-        id: "transformation-config",
+        id: "choice-config",
         tag: "form",
         classes: ["sheet", "dnd5e2", "standard-form"],
         window: {
-            title: "Transformation",
+            title: "Transformation Stage Choice",
             width: 600,
             resizable: false
         },
         actions: {
-            save: TransformationConfig._onSave
+            save: ChoiceDialogConfig._onSave
         }
     };
 
-    constructor(actor, transformations, options = {}) {
+    constructor(choices, options = {}) {
         super(options);
-        this.actor = actor;
-        this.transformations = transformations;
-        this._initialStage = actor.getFlag("dnd5e", "transformationStage") ?? null;
-        this._initialTransformation = actor.getFlag("dnd5e", "transformation") ?? null;
+        this.choices = choices;
     }
 
     get isEditable() {
@@ -27,51 +24,33 @@ export class TransformationConfig extends foundry.applications.api.HandlebarsApp
 
     static PARTS = {
         content: {
-            template: "modules/transformations/scripts/Templates/transformationConfig.hbs"
+            template: "modules/transformations/scripts/Templates/choiceDialog.hbs"
         }
     };
 
     async _prepareContext() {
-        if (!this.transformations) throw new Error("Invalid transformations");
-        const selectedId = this.actor.getFlag("dnd5e", "transformation");
-        const actorTransformationStage = this.actor.getFlag("dnd5e", "transformationStage");
+        if (!this.choices) throw new Error("Invalid choices");
         const valueMap = [];
-        for (const transformation of TransformationModule.Transformations.values()) {
+        for (const choice of this.choices) {
             valueMap.push({
-                id: transformation.itemId,
-                name: transformation.name,
-                img: transformation.img,
-                selected: transformation.itemId === selectedId
+                icon: choice.itemId,
+                label: choice.label,
+                name: choice.name,
+                hint: choice.hint,
             });
         }
 
-        const rows = Math.ceil((TransformationModule.Transformations.length + 1) / 2);
+        const rows = Math.ceil((choices.length + 1) / 2);
 
         return {
-            editable: this.isEditable,
-            transformations: valueMap  ?? [],
-            transformationStage: actorTransformationStage ?? 0,
-            rows: rows ?? 1,
-            noneSelected: !selectedId,
-            transformationStages: CONFIG.DND5E.characterFlags.transformationStage ?? {}
+           choices: valueMap  ?? [],
+           rows: rows ?? 1,
         };
     }
 
     static async _onSave(event, target) {
         const app = this;
-        if (!app.isEditable) return;
         const formData = new FormData(target.form);
-        const id = formData.get("transformation");
-        if (id !== app._initialTransformation) {
-            if (id && id !== "None") {
-                await app.actor.setFlag("dnd5e", "transformation", id, { transformationsInternal: true });
-            } else {
-                await app.actor.unsetFlag("dnd5e", "transformation" , { transformationsInternal: true });
-            }
-        }
-        if (formData.get("transformationStage") != app._initialStage) {
-            await app.actor.setFlag("dnd5e", "transformationStage", formData.get("transformationStage"), { transformationsInternal: true });
-        }
         app.close();
     }
 }
