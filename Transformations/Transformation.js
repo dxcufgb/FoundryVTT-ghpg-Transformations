7c8214c6f7337400ec8c15f783f44dccb860333b
dxcufgb <dxcufgb@users.noreply.github.com>
2026-01-03 00:24:37 +0100

    [AH] attempt to make a choice dialog

--- Transformations/Transformation.js
+++ Transformations/Transformation.js
@@ -262,7 +262,7 @@ export class Transformation {
                 });
                 const choiceKey = `stage${this.transformationStage}ChoiceMade`;
                 if (!(this.actor.getFlag(TransformationModule.constants.EFFECT_FLAG_MODULE_NAME, choiceKey))) {
-                    const choice = await TransformationModule.utils.renderTransformationTemplate("ChoiceDialog", choices);
+                    const choice = await TransformationModule.utils.renderTransformationTemplate("choiceDialog", choices);
                     await this.actor.setFlag(TransformationModule.constants.EFFECT_FLAG_MODULE_NAME, choiceKey, choice.name);
                     await this.addItemToActor(choice);
                 } else {
